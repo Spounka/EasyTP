181d42e421b0ba4693e64e1b0c3025280ce39e38
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-11 19:55:24 +0100

    Added ValidationRules

---
+++ Student/ValidationRules/IPRule.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace Student.ValidationRules
+{
+    public class IPRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var ip = (string)value;
+            if (string.IsNullOrEmpty(ip) || string.IsNullOrWhiteSpace(ip))
+                return new ValidationResult(false, "IP cannot be empty");
+
+            var reg = new Regex(@"^([0-9]{1,3}\.){3}[0-9]{1,3}$");
+            var match = reg.Match(ip);
+            return match.Success
+                ? ValidationResult.ValidResult
+                : new ValidationResult(false, "IP address invalid");
+        }
+    }
+}
