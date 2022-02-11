0139ae8712b152e705ac16edf528ca085c797f28
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-11 19:57:12 +0100

    Project  Files

---
+++ Student/View/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Student.ViewModel;
+
+namespace Student.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private TcpConnectionVM _connectionVm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _connectionVm = new TcpConnectionVM();
+        }
+
+        private void OnConnectButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (!_connectionVm.IsIPValid(IPField.Text))
+            {
+                MessageBox.Show("IP / Host invalid");
+                return;
+            }
+
+            if (!int.TryParse(PortField.Text, out var port))
+            {
+                MessageBox.Show("IP / Host invalid");
+                return;
+            }
+
+            if (!_connectionVm.Connect(IPField.Text, port, out var errorMessage))
+            {
+                MessageBox.Show($"Error connecting to server: {errorMessage}");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(FullNameField.Text) || string.IsNullOrEmpty(FullNameField.Text))
+            {
+                MessageBox.Show("FullName cannot be empty");
+                return;
+            }
+
+            _connectionVm.SendUsername(FullNameField.Text);
+        }
+
+        private void OnDisconnectButtonClicked(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ Teacher/View/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using Teacher.ViewModel;
+
+namespace Teacher.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private ChatServerVM vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.SizeChanged += OnWindowResize;
+            vm = new ChatServerVM();
+            DataContext = vm;
+        }
+
+        private void OnWindowResize(object sender, SizeChangedEventArgs e)
+        {
+            vm.OnWindowResize(e.NewSize.Height, e.NewSize.Width);
+        }
+
+        private void StartServer(object sender, RoutedEventArgs e)
+        {
+            var portNumberText = PortNumberTextBox.Text;
+            if (string.IsNullOrEmpty(portNumberText) || string.IsNullOrWhiteSpace(portNumberText))
+                portNumberText = PortNumberTextBox.Text = "8000";
+
+            var success = int.TryParse(portNumberText, out var portNumber);
+            if (!success)
+                MessageBox.Show("Error, Input a valid Port number!", "EasyTP - Teacher");
+            else
+                vm.StartListening(portNumber);
+        }
+
+        private void StopServer(object sender, RoutedEventArgs e)
+        {
+            vm.StopServer();
+        }
+    }
+}
