d269626f4bd9dec3dd388e0b707f975dab07a647
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-11 19:55:40 +0100

    Added ValidationRules Tests

---
+++ Student.Tests/ValidationRules/FullNameRuleTest.cs
@@ -0,0 +1,122 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+using Student.ValidationRules;
+
+namespace Student.Tests.ValidationRules
+{
+    [TestFixture]
+    public class FullNameRuleTest
+    {
+        [Test]
+        public void FullName_EmptyString_FalseTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("", culture);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_WhiteSpacesString_FalseTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("\t", culture);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_NoSpaces_FalseTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("NazihBoudaakkar", culture);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_ValidNameString_TrueTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("Nazih Boudaakkar", culture);
+
+            // ASSERT
+            Assert.IsTrue(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_ValidNameWithNumbers_FalseTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("Nazih 22Boudaakkar", culture);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_ThreeNames_TrueTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("Nazih Von Boudaakkar", culture);
+
+            // ASSERT
+            Assert.IsTrue(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_OneName_FalseTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("Nazih", culture);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void FullName_FourNames_FalseTest()
+        {
+            // ARRANGE
+            var fullNameTestRule = new FullNameRule();
+            var culture = new CultureInfo("ar-DZ", false);
+
+            // ACT
+            var validationResult = fullNameTestRule.Validate("Nazih Spounka Von Schpeiner", culture);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+    }
+}
---
+++ Student.Tests/ValidationRules/PortRuleTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using NUnit.Framework;
+using Student.ValidationRules;
+
+namespace Student.Tests.ValidationRules
+{
+    [TestFixture]
+    public class PortRuleTest
+    {
+        private PortRule validationRule;
+        private CultureInfo _cultureInfo;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            validationRule = new PortRule();
+            _cultureInfo = new CultureInfo("ar-DZ", false);
+        }
+
+        [Test]
+        public void PortRule_Characters_FalseTest([Values("a", "28b", "31cb", "_28zx.")] string port)
+        {
+            // ACT
+            var validationResult = validationRule.Validate(port, _cultureInfo);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void PortRule_EmptyWhiteSpaceNull_FalseTest([Values("", " ", "\t", "\n", null)] string port)
+        {
+            // ACT
+            var validationResult = validationRule.Validate(port, _cultureInfo);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void PortRule_PortUnderRange_FalseTest()
+        {
+            var port = (IPEndPoint.MinPort - 1).ToString();
+            // ACT
+            var validationResult = validationRule.Validate(port, _cultureInfo);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void PortRule_PortOverRange_FalseTest()
+        {
+            var port = (IPEndPoint.MaxPort + 1).ToString();
+            // ACT
+            var validationResult = validationRule.Validate(port, _cultureInfo);
+
+            // ASSERT
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void PortRule_PortValid_TrueTest()
+        {
+            var port = new Random().Next(IPEndPoint.MinPort, IPEndPoint.MaxPort + 1).ToString();
+            // ACT
+            var validationResult = validationRule.Validate(port, _cultureInfo);
+
+            // ASSERT
+            Assert.IsTrue(validationResult.IsValid);
+        }
+    }
+}
