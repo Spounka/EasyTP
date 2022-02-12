a2f8869d04a204ca773968f4c08f1aaf9f0cb5cc
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-12 14:11:04 +0100

    Added StateObject for communication

---
+++ Teacher/Model/StateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Teacher.Model
+{
+    [System.Serializable]
+    public class StateObject
+    {
+        public const int BufferSize = 1024;
+        public byte[] Buffer = new byte[BufferSize];
+
+        public override string ToString()
+        {
+            return Encoding.UTF8.GetString(Buffer);
+        }
+    }
+}
