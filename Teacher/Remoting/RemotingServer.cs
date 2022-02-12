381e17966e4c93d8ee28639b41e5cd7c3dd81e15
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-12 18:33:32 +0100

    added remoting server

---
+++ Teacher/Remoting/RemotingServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using Teacher.Remoting.Implementations;
+
+namespace Teacher.Remoting
+{
+    public static class RemotingServer
+    {
+        public static void StartRemotingServer(int port)
+        {
+            var channel = new TcpChannel(port);
+
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(ValidatePresenceImpl), "presence",
+                WellKnownObjectMode.Singleton);
+        }
+    }
+}
